603e9513c8d742742618c2ea8a5fffad2440a409
Michael Palldino <michael.a.palladino@rutgers.edu>
2022-06-27 22:22:01 -0400

    adjusted connection

--- src/lib/utils/getConnection.ts
+++ src/lib/utils/getConnection.ts
@@ -3,6 +3,6 @@ import { Connection as MakeConnection } from '../Connection';
 
 export const getConnection = (url?: string) => {
 	if (browser)
-		return new MakeConnection(url ?? 'https://dax-server.michaelpalladino.io') as MakeConnection;
+		return new MakeConnection(url ?? 'https://dax.server.michaelpalladino.io') as MakeConnection;
 	else throw new Error('Connection can only be used in browser');
 };
