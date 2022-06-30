04e5aea5a9d5d4f871a3b034df0ed6b6c380e614
Michael Palldino <michael.a.palladino@rutgers.edu>
2022-06-30 15:52:03 -0400

    fixed build url

--- src/lib/space/makeHumanModel.ts
+++ src/lib/space/makeHumanModel.ts
@@ -37,7 +37,7 @@ export const makeHumanModel = ({
 	//   polarGridHelper.position.x = 200;
 	scene.add(polarGridHelper);
 
-	const url = `https://dax-mobile.michaelpalladino.io/assets/models/LeePerrySmith.glb`;
+	const url = `https://dax.michaelpalladino.io/assets/models/LeePerrySmith.glb`;
 
 	loader.load(url, (gltf) => {
 		const mesh = gltf.scene.children[0] as THREE.Mesh;
